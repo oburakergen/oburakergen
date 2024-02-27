1a5b774201b6bf2d0677f02c247aa87162bce8a4
Burak ERGEN <burak.ergen@armagroupholding.com>
2024-02-27 12:56:19 +0300

    added error-handler.ts

--- plugins/error-handler.ts
+++ plugins/error-handler.ts
@@ -1,11 +1,11 @@
-// // eslint-disable-next-line @typescript-eslint/no-unused-vars
-// export default defineNuxtPlugin((nuxt) => {
-//   nuxt.vueApp.config.errorHandler = (error, instance, info) => {
-//     // handle error, e.g. report to a service
-//   };
-//
-//   // Also possible
-//   nuxt.hook("vue:error", (error, instance, info) => {
-//     // handle error, e.g. report to a service
-//   });
-// });
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default defineNuxtPlugin((nuxt) => {
+  nuxt.vueApp.config.errorHandler = (error, instance, info) => {
+    // handle error, e.g. report to a service
+  };
+
+  // Also possible
+  nuxt.hook("vue:error", (error, instance, info) => {
+    // handle error, e.g. report to a service
+  });
+});
